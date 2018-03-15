38b08a0fe2d0004cbba425a75798ba126837dadc
Nick Schoonens <nick@schoonzie.com>
2018-03-15 16:41:51 +1000

    Allowing simplesamlphp to work with lando and nginx.

--- www/_include.php
+++ www/_include.php
@@ -1,5 +1,31 @@
 <?php
 
+// nginx doesn't populate the $_SERVER['PATH_INFO'] variable which is used a lot
+// in this package. The below hack populates the value to make it work.
+// @see https://github.com/simplesamlphp/simplesamlphp/issues/5
+if (empty($_SERVER['PATH_INFO'])) {
+  $_SERVER['PATH_INFO'] = '';
+
+  $matches = array();
+  if (preg_match("/^(.+\/module\.php)(\/.+)$/", $_SERVER['REQUEST_URI'], $matches)) {
+    $pathinfo = $matches[2];
+
+    $matches = array();
+    if (preg_match("/(.*)\?(.*)/", $pathinfo, $matches)) {
+      $pathinfo = $matches[1];
+    }
+
+    $_SERVER['PATH_INFO'] = $pathinfo;
+  }
+}
+
+// Below fixes an issue with lando edge urls being redirected with :80 appended.
+// Eg: https://site:80. The below hack forces the correct port.
+// @see: https://github.com/simplesamlphp/simplesamlphp/issues/450
+if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] = 'on') {
+  $_SERVER['SERVER_PORT'] = '443';
+}
+
 // initialize the autoloader
 require_once(dirname(dirname(__FILE__)).'/lib/_autoload.php');
 
